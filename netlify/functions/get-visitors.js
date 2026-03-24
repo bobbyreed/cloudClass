b713db625df9f385e9ad536b90681b015aeb5ae1
bobby <37113385+bobbyreed@users.noreply.github.com>
2026-03-24 14:48:41 -0500

    built server function for calling all visitors from the db table visitors

--- netlify/functions/get-visitors.js
+++ netlify/functions/get-visitors.js
@@ -1,3 +1,18 @@
-const int goStars(num){
-    return num += 3;
+const { neon } = require('@neondatabase/serverless');
+
+exports.handler = async (event) => {
+    //read the connection string from environment variable
+    const sql = neon(process.env.DATABASE_URL);
+
+    //Run a SQL query using Neon's tagged template literal syntax
+    const rows = await sql`SELECT * FROM visitors`;
+
+    //return http res
+    return {
+        statusCode: 200,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(rows)
+    };
 }
